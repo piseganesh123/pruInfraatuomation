b2f0cf53ba67df50866154599a731442d4367816
piseganesh123 <piseganesh123@gmail.com>
2018-11-04 19:41:12 +0530

    Update quickStart.sh
    added docker command to start Jenkins container

--- quickStart.sh
+++ quickStart.sh
@@ -13,13 +13,13 @@ gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
-#Run Docker’s installation script:
+#Run Dockerâ€™s installation script:
 yum -y install git docker
 #wget and java required for local Jenkins installatnion only, for dockerised version this is not required
 yum install wget java
 #docker-engine-1.12.6-1.el7.centos.x86_64
 systemctl start docker
 
-#Build emoji image
-sudo docker build . -t emoji-test:2
-docker run -it -p 3000:3000 emoji-test:2
+
+#Start Jenkins container
+sudo docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 --volume=/var/run/docker.sock:/var/run/docker.sock -v /root/jenkins_2112:/var/jenkins_home -v $(which docker):/usr/bin/docker -v /root/jenkins_2112:/var/jenkins_home jenkins/jenkins
